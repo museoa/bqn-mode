a437c1f2fd9eeba4f302840b15b07209ff38b20a
AndersonTorres <torres.anderson.85@protonmail.com>
2022-11-20 11:11:10 -0300

    [task] add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((fill-column . 80)
+		             (indent-tabs-mode . nil)
+		             (sentence-end-double-space . nil))))
