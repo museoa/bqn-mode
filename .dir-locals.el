32e91f39bcb7f170be05ca94853dadeb98f7ba44
Anderson Torres <torres.anderson.85@protonmail.com>
2023-03-07 22:19:33 -0300

    [fixup] .dir-locals.el: whitespace cleanup

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,5 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((emacs-lisp-mode . ((fill-column . 80)
-		             (indent-tabs-mode . nil)
-		             (sentence-end-double-space . nil))))
+                     (indent-tabs-mode . nil)
+                     (sentence-end-double-space . nil))))
