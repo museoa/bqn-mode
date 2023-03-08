b594b3e682aeda114982d283271686c2f7086599
Anderson Torres <torres.anderson.85@protonmail.com>
2023-03-07 21:20:08 -0300

    [fixup] bqn-mode.el: fix docstrings

--- bqn-mode.el
+++ bqn-mode.el
@@ -16,7 +16,7 @@
 
 ;; There are two ways to access the BQN keymap:
 ;; - When editing a BQN file, use keys with the super (s-) modifier.
-;; - Enable backslash prefixes by entering C-\ (‘toggle-input-method’)
+;; - Enable backslash prefixes by entering C-\ (`toggle-input-method')
 ;;   then BQN-Z. Then enter backslash \ before a key.
 
 ;;; Code:
