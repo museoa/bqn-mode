d66fe13e5cf98550997aef7e6856677e7199d31d
Ingo Lohmar <ingo.lohmar@posteo.net>
2023-04-22 10:50:30 +0200

    fix syntax-table (messed up when deriving symbols list differently)

--- bqn-mode.el
+++ bqn-mode.el
@@ -147,7 +147,7 @@
 
 (defvar bqn-syntax--table
   (let ((table (make-syntax-table)))
-    (dolist (s (bqn--symbols-no-doc 'all))
+    (dolist (s (bqn--symbols-no-doc))
       (modify-syntax-entry (cdr s) "." table))
     (dolist (s (string-to-list "$%&*+-/<=>|"))
       (modify-syntax-entry s "." table))
